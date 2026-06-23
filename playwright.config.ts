a4e6a748bf38b8f7806aead58e5c9d3cc24a8320
Anurag Malviya <anuragmalviya90@gmail.com>
2026-06-23 16:47:48 +0530

    Fix Issue

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,18 @@ export default defineConfig({
   },
   retries: 0,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [['html'], ['github'], ['allure-playwright']],
+  reporter: [
+    ['html'],
+    ['github'],
+    [
+      'allure-playwright',
+      {
+        outputFolder: 'allure-results',
+        detail: true,
+        suiteTitle: false,
+      },
+    ],
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
